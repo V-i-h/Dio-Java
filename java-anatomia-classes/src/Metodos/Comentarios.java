174cd7c1a1ce5acfaeac804602fb4a6f54c9dfe8
Vitória Silva <vitoriasilvan321@gmail.com>
2024-06-06 15:50:41 -0300

    metodos

---
+++ java-anatomia-classes/src/Metodos/Comentarios.java
@@ -0,0 +1,23 @@
+package Metodos;
+
+public class Comentarios {
+    public static void main(String[] args) {
+    //One line
+    // Olá, eu sou um comentário em uma única linha
+    
+    //Mult line
+    /* Olá,
+    * Eu sou um comentario
+    * que posso ser mais detalhadod
+    * quando necessário
+    */
+    }
+    //Documentation
+    /** 
+ * Estas duas estrelinhas acima
+ * é para identificar que você
+ * pretende elaborar um comentário
+ * a nível de documentação.
+ * Que incrível !!!
+ */
+}
---
+++ java-anatomia-classes/src/Metodos/PalavrasReservadas.java
@@ -0,0 +1,89 @@
+package Metodos;
+
+public class PalavrasReservadas {
+  public static void main(String[] args) {
+    //Palavras reservadas são como identificadores
+    //de uma linguagem que já possuem uma finalidade 
+    //!!Não podem ser utilizadas para nomear variáveis, classes, métodos ou atributos
+   
+  //abstract    continue    goto           package    synchronized
+  // assert      default     if             private    this
+  // boolean     do          implements     protected  throw
+  // break       double      import         public     throws
+ //  byte        else        instanceof     return     transient
+  // case        extends     int            short      try
+ // catch        final        interface      static     void
+ //  char        finally     long            super      while
+  // class       float       new              switch      
+  // const       for                         
+
+    //Controle de pacotes
+    //import 
+    //package 
+
+    //Modificadores de acesso
+    //public
+    //private
+    //protected
+
+    //PRIMITIVOS
+    //boolean
+    //byte
+    //char
+    //double
+    //float
+    //int
+    //long
+    //short
+    //void
+
+    //Modificadores de classes, variáveis ou métodos
+    //abstract
+    //class
+    //extends
+    //final
+    //implements
+    //interface
+    //new
+    //static
+    //synchronized
+    //transient
+
+    //Controle de fluxo dentro de um bloco de código
+    //break
+    //case
+    //continue
+    //default
+    //do
+    //else
+    //for
+    //if
+    //instanceof
+    //return
+    //switch
+    //while
+     
+    //Tratamento de erros
+    //assert
+    //catch
+    //finally
+    //throw
+    //throws
+    //try
+
+    //Variáveis de referência
+    //super
+    //this
+
+
+    //Palavras reservadas não utilizadas
+    //const
+    //goto
+
+
+
+
+}
+
+    
+}
